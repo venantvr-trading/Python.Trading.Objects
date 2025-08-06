098eda0f7bece22f7884fdce8e96a599e6506303
rvv <r.venant.valery@gmail.com>
2025-08-06 10:40:47 +0200

    First commit...

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Python.Trading.Objects",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Une bibliothèque Python pour les objets de trading.",
+    author="venantvr",
+    author_email="venantvr@gmail.com",
+    url="https://github.com/venantvr/Python.Trading.Objects",
+    install_requires=[],  # Ajoutez vos dépendances ici si nécessaire
+    python_requires='>=3.8',
+)
