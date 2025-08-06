ff2c8d4f60658e64fe0dea04cf8a1bd9e95a29cd
rvv <r.venant.valery@gmail.com>
2025-08-06 13:19:33 +0200

    First commit...

---
+++ quotes/__init__.py
@@ -0,0 +1,15 @@
+from .assertion import bot_assert
+from .coin import Token
+from .pair import BotPair
+from .price import Price
+from .quote import Quote
+from .usd import USD
+
+__all__ = [
+    "bot_assert",
+    "Token",
+    "BotPair",
+    "Price",
+    "Quote",
+    "USD",
+]
